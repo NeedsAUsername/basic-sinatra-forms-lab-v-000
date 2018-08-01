26150a93d21306696cf0c856ce6bb8622a9695ce
Kevin Chi <kevinchi118@gmail.com>
2018-08-01 02:26:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,10 +9,10 @@ class App < Sinatra::Base
 
   post '/team' do
     @coach = params[:coach]
-    @point_guard = params[:point_guard]
-    @shooting_guard = params[:shooting_guard]
-    @small_forward = params[:small_forward]
-    @power_forward = params[:power_forward]
+    @point_guard = params[:pg]
+    @shooting_guard = params[:sg]
+    @small_forward = params[:sf]
+    @power_forward = params[:pf]
     @center = params[:center]
 
     erb :team
